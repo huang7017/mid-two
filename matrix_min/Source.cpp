7a8b4b6b487f359d0bc1fb599da76b90dd8d322f
huang7017 <huang7017@gmail.com>
2019-05-08 15:46:47 +0800

    first commit

--- matrix_min/Source.cpp
+++ matrix_min/Source.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+int min(vector<vector<int>> list) {
+	list.resize(4);
+	for (int i = 1; i < 4; i++)list[i].resize(3);
+	int min = list[0][0];
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (list[i][j] <= min) min = list[i][j];
+		}
+	}
+
+	return min;
+}
 int main() {
 	vector<vector<int>> array2d(vector<vector<int>>(4, vector<int>(3, 0)));
 	for (int i = 0; i < 4; i++) {
@@ -11,14 +23,8 @@ int main() {
 		}
 	}
 
-	int min = array2d[0][0];
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 3; j++) {
-			if(array2d[i][j] <= min) min = array2d[i][j];
-		}
-	}
 
-	cout << "min: " << min << endl;
+	cout << "min: " << min(array2d) << endl;
 
 	system("pause");
 }
